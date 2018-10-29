b4b7cf781969184419871c3d79c397c2e9097e23
nanmu42 <i@nanmu.me>
2018-10-29 16:10:08 +0800

    [fix] build script

--- cmd/api/build.sh
+++ cmd/api/build.sh
@@ -6,4 +6,4 @@ BUILD=`date +%FT%T%z`
 # can not build statically safely
 # see https://stackoverflow.com/questions/8140439/why-would-it-be-impossible-to-fully-statically-link-an-application
 export LD_LIBRARY_PATH=/usr/local/lib
-go build -ldflags '-s -w -X main.Version=$VERSION -X main.BuildDate=$BUILD' -o qrcode-api
+go build -ldflags "-s -w -X main.Version=$VERSION -X main.BuildDate=$BUILD" -o qrcode-api
